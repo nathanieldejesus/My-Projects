7b432628ea46108e651febca7c513bbf0c3d1f16
nathanieldejesus <112787893+nathanieldejesus@users.noreply.github.com>
2023-01-23 18:04:39 -0700

    allows for manual directory selection

--- yt_downloader.py
+++ yt_downloader.py
@@ -4,8 +4,23 @@ import os
 
 # base for GUI
 base = Tk()
-base.geometry("400x225")
+base.geometry("400x300")
 base.title("Downloader")
+
+
+def check_directory():
+    # checks if directory link inputted in text box
+    if not dir.get():
+        return False
+    else:
+        return True
+
+
+def convert_mp3(file_out):
+    # converts download to .mp3
+    name, ext = os.path.splitext(file_out)
+    file_rename = name + ".mp3"
+    os.rename(file_out, file_rename)
 
 
 def loadmp3():
@@ -15,10 +30,13 @@ def loadmp3():
         myVar.set("Downloading...")
         base.update()
         vid = YouTube(link.get()).streams.filter(only_audio=True).first()
-        file_out = vid.download()
-        name, ext = os.path.splitext(file_out)
-        file_rename = name + ".mp3"
-        os.rename(file_out, file_rename)
+        dircheck = check_directory()
+        if not dircheck:
+            file_out = vid.download()
+            convert_mp3(file_out)
+        else:
+            file_out = vid.download(dir.get())
+            convert_mp3(file_out)
         myVar.set("Download successful")
     except Exception as e:
         myVar.set("Download Error")
@@ -32,7 +50,11 @@ def loadmp4():
         myVar.set("Downloading...")
         base.update()
         vid = YouTube(link.get()).streams.get_highest_resolution()
-        vid.download()
+        dircheck = check_directory()
+        if not dircheck:
+            vid.download()
+        else:
+            vid.download(dir.get())
         myVar.set("Download successful")
     except Exception as e:
         myVar.set("Download Error")
@@ -43,11 +65,22 @@ def loadmp4():
 # generate the input box, text on GUI
 my_font = "SFPro 15 bold"
 Label(base, text="YouTube Downloader", font=my_font).pack()
+
+# entry box for video link
 myVar = StringVar()
 myVar.set("Enter link below")
 Label(base, textvariable=myVar, font=my_font).pack(pady=5)
 link = StringVar()
 Entry(base, textvariable=link, width=35).pack(pady=7)
+
+# entry box for directory link
+dir_label = StringVar()
+dir_label.set("(Optional) Enter directory")
+Label(base, textvariable=dir_label, font=my_font).pack(pady=7)
+dir = StringVar()
+Entry(base, textvariable=dir, width=35).pack(pady=7)
+
+# buttons to initiate download
 Button(base, text="Download as .mp3", command=loadmp3).pack(pady=10)
 Button(base, text="Download as .mp4", command=loadmp4).pack(pady=7)
 
